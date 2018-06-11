3ca32154187ad5c14474770f8fb8dfbc4757ca1d
Brandon <bhanks@mit.edu>
2018-06-11 14:58:54 -0400

    added working perspective button

---
+++ MITGeometry/Assets/_Scripts/Perspective.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perspective : MonoBehaviour {
+
+    public Camera cam;
+    bool ortho;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+
+	public void changePerspective () { 
+        cam.orthographic = !cam.orthographic;
+    }
+}
