308bc49352b9e7a0b6d2f18bf67d6b201eb7863c
Brandon <bhanks@mit.edu>
2018-06-12 12:22:08 -0400

    walls should be hidden in perspective mode now

--- MITGeometry/Assets/_Scripts/Perspective.cs
+++ MITGeometry/Assets/_Scripts/Perspective.cs
@@ -6,14 +6,21 @@ public class Perspective : MonoBehaviour {
 
     public Camera cam;
     bool ortho;
+    GameObject[] walls;
 
     void Start()
     {
         cam = Camera.main;
+        walls = GameObject.FindGameObjectsWithTag("Walls");
     }
 
 
 	public void changePerspective () { 
+
+        foreach (GameObject wall in walls)
+        {
+            wall.GetComponent<MeshRenderer>().enabled = !wall.GetComponent<MeshRenderer>().enabled;
+        }
         cam.orthographic = !cam.orthographic;
     }
 }
