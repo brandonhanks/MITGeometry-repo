01d524042ffb1fa13075ae5415a493d71344d961
Brandon <bhanks@mit.edu>
2018-06-13 21:22:07 -0400

    rotate buttons in and working now

---
+++ MITGeometry/Assets/_Scripts/RotateButtons.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateButtons : MonoBehaviour {
+
+	public GameObject cube;
+    private int delta = 5;
+
+	void Update () {
+
+		cube = SelectionRaycast.selectedGameobject; //gameobject to move is the one being hit by raycast
+	
+	}
+
+	public void movePlusX () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.x += delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving X rotation
+    }
+
+    public void moveMinusX () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.x -= delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving X rotation
+    }
+
+
+	public void movePlusY () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.y += delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving Y rotation
+    }
+
+    public void moveMinusY () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.y -= delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving Y rotation
+    }
+
+
+	public void movePlusZ () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.z += delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving Z rotation
+    }
+
+    public void moveMinusZ () {
+        Vector3 pos = cube.transform.rotation.eulerAngles;
+        pos.z -= delta;
+        cube.transform.rotation = Quaternion.Euler(pos.x, pos.y, pos.z); //moving Z rotation
+    }
+}
