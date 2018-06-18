d8c6f80011607c7b0ddadf19a97607bb02298018
Brandon <bhanks@mit.edu>
2018-06-18 13:14:17 -0400

    regrouped some objects, working on translation deltas too

--- MITGeometry/Assets/_Scripts/MoveInputButton.cs
+++ MITGeometry/Assets/_Scripts/MoveInputButton.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveInputButton : MonoBehaviour {
 
 	public GameObject cube;
+    private int delta = 5;
 
 	void Update () {
 
@@ -14,39 +15,39 @@ public class MoveInputButton : MonoBehaviour {
 
 	public void movePlusX () {
         Vector3 pos = cube.transform.position;
-        pos.x += 10;
+        pos.x += delta;
         cube.transform.position = pos; //moving X position
     }
 
     public void moveMinusX () {
         Vector3 pos = cube.transform.position;
-        pos.x -= 10;
+        pos.x -= delta;
         cube.transform.position = pos; //moving X position
     }
 
 
 	public void movePlusY () {
         Vector3 pos = cube.transform.position;
-        pos.y += 10;
+        pos.y += delta;
         cube.transform.position = pos; //moving Y position
     }
 
     public void moveMinusY () {
         Vector3 pos = cube.transform.position;
-        pos.y -= 10;
+        pos.y -= delta;
         cube.transform.position = pos; //moving Y position
     }
 
 
 	public void movePlusZ () {
         Vector3 pos = cube.transform.position;
-        pos.z += 10;
+        pos.z += delta;
         cube.transform.position = pos; //moving Z position
     }
 
     public void moveMinusZ () {
         Vector3 pos = cube.transform.position;
-        pos.z -= 10;
+        pos.z -= delta;
         cube.transform.position = pos; //moving Z position
     }
 }
