c156e61d7196fb27e30e430d196f83ec00090f0b
Brandon <bhanks@mit.edu>
2018-06-18 17:05:56 -0400

    finalizing things with the new camera controls

---
+++ MITGeometry/Assets/_Scripts/CameraControls.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+
+    public Transform target;
+
+    public float horizontalMovement = 45;
+    public float verticalMovement = 45;
+	public Camera cam;
+	Vector3 defaultrot;
+	Vector3 defaultpos;
+
+	void Start () {
+		defaultpos = cam.transform.position;
+		defaultrot = cam.transform.eulerAngles;
+	}
+
+    public void MoveCam(string direction)
+    {
+     
+		switch (direction)
+		{
+			case "left":
+				cam.transform.RotateAround(target.position, Vector3.up, horizontalMovement * -1.0f);
+				break;
+			case "right":
+				cam.transform.RotateAround(target.position, Vector3.up, horizontalMovement);
+				break;
+			case "down":
+				float newDownPitch = cam.transform.eulerAngles.x + verticalMovement;
+				cam.transform.eulerAngles = new Vector3 (newDownPitch, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+				// cam.transform.RotateAround(target.position, Vector3.right, verticalMovement * -1.0f);
+				break;
+			case "up":
+				float newUpPitch = cam.transform.eulerAngles.x - verticalMovement;
+				cam.transform.eulerAngles = new Vector3 (newUpPitch, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+				// cam.transform.eulerAngles.x += verticalMovement;
+				// cam.transform.RotateAround(target.position, Vector3.right, verticalMovement);
+				break;
+			case "home":
+				cam.transform.position = defaultpos;
+				cam.transform.eulerAngles = defaultrot;
+				break;
+			default:
+				break;
+		}
+
+
+
+
+
+    }
+
+}
+
+
+	// // Use this for initialization
+
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+// }
