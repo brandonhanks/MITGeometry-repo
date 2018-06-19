3a42533fecec8b789b249fd32f325e5218408433
Brandon <bhanks@mit.edu>
2018-06-19 01:16:28 -0400

    fixed camera issues

--- MITGeometry/Assets/_Scripts/CameraControls.cs
+++ MITGeometry/Assets/_Scripts/CameraControls.cs
@@ -7,8 +7,13 @@ public class CameraControls : MonoBehaviour {
 
     public Transform target;
 
+<<<<<<< HEAD
+    public float horizontalMovement = 15;
+    public float verticalMovement = 15;
+=======
     public float horizontalMovement = 45;
     public float verticalMovement = 45;
+>>>>>>> 5fecf5d806831ffa44f7d69a229213c20785ecb3
 	public Camera cam;
 	Vector3 defaultrot;
 	Vector3 defaultpos;
@@ -32,13 +37,26 @@ public class CameraControls : MonoBehaviour {
 			case "down":
 				// float newDownPitch = cam.transform.eulerAngles.x + verticalMovement;
 				// cam.transform.eulerAngles = new Vector3 (newDownPitch, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+<<<<<<< HEAD
+				if (cam.transform.eulerAngles.x > 10) {
+					cam.transform.RotateAround(target.position, cam.transform.right, verticalMovement * -1.0f);
+				}
+				
+=======
 				cam.transform.RotateAround(target.position, cam.transform.right, verticalMovement * -1.0f);
+>>>>>>> 5fecf5d806831ffa44f7d69a229213c20785ecb3
 				break;
 			case "up":
 				// float newUpPitch = cam.transform.eulerAngles.x - verticalMovement;
 				// cam.transform.eulerAngles = new Vector3 (newUpPitch, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
 				// cam.transform.eulerAngles.x += verticalMovement;
+<<<<<<< HEAD
+				if (cam.transform.eulerAngles.x < 80) {
+					cam.transform.RotateAround(target.position, cam.transform.right, verticalMovement);
+				}
+=======
 				cam.transform.RotateAround(target.position, cam.transform.right, verticalMovement);
+>>>>>>> 5fecf5d806831ffa44f7d69a229213c20785ecb3
 				break;
 			case "home":
 				cam.transform.position = defaultpos;
