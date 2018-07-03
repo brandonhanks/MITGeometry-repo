accee077c1f681dd9ff6d1b32782e7bfc7430877
Brandon <bhanks@mit.edu>
2018-07-02 20:47:17 -0400

    added session support and related reporting functions

---
+++ MITGeometry/Assets/_Scripts/Reporter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEditor;
+using UnityEngine.Networking;
+
+// [InitializeOnLoad]
+public class Reporter : MonoBehaviour {
+
+	public string sessionID;
+
+	void Start () {
+		StartCoroutine(getSessionID());
+	}
+
+	IEnumerator getSessionID () {
+		UnityWebRequest session = UnityWebRequest.Get("http://gbakimchi.herokuapp.com/");
+		yield return session.SendWebRequest();
+		string cookieHeader = session.GetResponseHeader("Set-Cookie");
+		string[] cookies = cookieHeader.Split(';');
+		foreach (var cookie in cookies)
+		{
+			if (cookie.StartsWith("sessionid"))
+			{
+				sessionID = cookie.Split('=')[1].Trim(';');
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Event (var params) {
+		WWWForm form = new WWWForm();
+		form.AddField("sessionID", sessionID);
+		// form.AddField("type", "pickup");
+		// form.AddField("context", "toygame");
+		// form.AddField("params", "{count: " + count.ToString() + "}" );
+		UnityWebRequest www = UnityWebRequest.Post("http://gbakimchi.herokuapp.com/api/event/", form);
+		www.SendWebRequest();
+	}
+	
+}
