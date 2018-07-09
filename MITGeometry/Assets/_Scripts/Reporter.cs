7ecc25010b408cbc8f3abd981eb5aa503524aa4a
Brandon <bhanks@mit.edu>
2018-07-09 08:46:58 -0400

    Reporter now has session id

--- MITGeometry/Assets/_Scripts/Reporter.cs
+++ MITGeometry/Assets/_Scripts/Reporter.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.Networking;
 
-[InitializeOnLoad]
 public class Reporter : MonoBehaviour {
 
 	public string sessionID;
