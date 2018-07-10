411217cd1c7d048f8fffcd435b08359673ac0a56
Brandon <bhanks@mit.edu>
2018-07-10 08:23:39 -0400

    regenerated unity metadata

--- MITGeometry/Assets/_Scripts/SceneLoader.cs
+++ MITGeometry/Assets/_Scripts/SceneLoader.cs
@@ -12,12 +12,12 @@ public class SceneLoader : MonoBehaviour {
 
 	private Color textColor;
 	
-	private GameObject reporter;
+	// private GameObject reporter;
 	// private string sessionid;
 
 	void Start() {
 		textColor = loadingText.color;
-		reporter = GameObject.Find("Reporter");
+		// reporter = GameObject.Find("Reporter");
 		// sessionid = reporter.GetComponent<Reporter>().sessionID;		
 	}
 
