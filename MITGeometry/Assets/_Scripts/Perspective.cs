f69c741162feeecd42b17b6afac51f3568c06384
Brandon <bhanks@mit.edu>
2018-07-11 10:01:16 -0400

    perspective reporting

--- MITGeometry/Assets/_Scripts/Perspective.cs
+++ MITGeometry/Assets/_Scripts/Perspective.cs
@@ -9,8 +9,22 @@ public class Perspective : MonoBehaviour {
     bool ortho;
     GameObject[] walls;
 
+    Reporter reporter;
+    class DataObj {
+        public string previous_perspective;
+        public string new_perspective;
+
+		public string ToJson()
+		{
+			return JsonUtility.ToJson(this);
+		}
+    }
+    DataObj data = new DataObj();
+	string type = "change_perspective";
+
     void Start()
     {
+        reporter = GameObject.Find("Reporter").GetComponent<Reporter>();
         // Ensures default config is set
         cam = Camera.main;
         cam.orthographic = false;
@@ -31,11 +45,17 @@ public class Perspective : MonoBehaviour {
             wall.GetComponent<MeshRenderer>().enabled = !wall.GetComponent<MeshRenderer>().enabled;
         }
         // Changes the perspective state to the opposite of what it was
+        
         cam.orthographic = !cam.orthographic;
         if (cam.orthographic){
             buttonLabel.GetComponent<Text>().text = "Orthographic";
+            data.previous_perspective = "perspective";
+            data.new_perspective = "orthographic";
         } else {
             buttonLabel.GetComponent<Text>().text = "Perspective";
+            data.previous_perspective = "orthographic";
+            data.new_perspective = "perspective";
         }
+        reporter.Event(type, data.ToJson());
     }
 }
